b9825d28a6bf78f3c7def9696dc3c76584afc539
sstrausman <sstrausman@gmail.com>
2015-09-16 14:09:19 -0500

    added more validations logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
 	validates :client_first_name, presence: true
 	validates :client_last_name, presence: true
-	validates :job_identification_number, uniqueness: true, on: { :create }
+	validates :job_identification_number, uniqueness: true, on: :create 
 	has_many :contacts
 	has_many :documents
 	has_many :notifications
