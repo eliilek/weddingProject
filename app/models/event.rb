f40e861a4c31186a7aca6b7a47abfe24cf791c22
eliilek <efklein@iastate.edu>
2017-03-10 17:28:33 -0600

    Company edit

--- app/models/event.rb
+++ app/models/event.rb
@@ -71,13 +71,19 @@ class Event < ActiveRecord::Base
     			else
 	    			self.primary_contact = "Client of " + self.planner_company
 	    		end
+				else
+					if self.company == "" or self.company == nil
+						self.client_first_name = self.company + " -"
+						self.client_last_name = self.primary_contact
+					else
+						name_array = self.primary_contact.split(" ")
+		    		first_name = name_array[0]
+		    		name_array.shift
+		    		second_name = name_array.join(" ")
+		    		self.client_first_name = first_name
+		    		self.client_last_name = second_name
+					end
     		end
-	    	name_array = self.primary_contact.split(" ")
-    		first_name = name_array[0]
-    		name_array.shift
-    		second_name = name_array.join(" ")
-    		self.client_first_name = first_name
-    		self.client_last_name = second_name
     	end
     end
 
